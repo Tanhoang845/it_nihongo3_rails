2b6a405c579643758c49331f385912863888e632
TANHOANG <tanhoang845@gMAIL.com>
2023-02-07 00:54:59 +0700

    AddHome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 <<<<<<< HEAD
 =======
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'login' => 'devise#users'
+  root 'static_pages#home'
   devise_for :users
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
